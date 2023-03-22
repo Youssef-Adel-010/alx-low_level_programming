b2f1d35ca1530c7a5a0e52bda57d75e8af622dea
youssef adel <yossefadel260@gmail.com>
2023-03-22 12:39:28 -0700

    words

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,7 +11,7 @@ void times_table(void)
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j += c)
+		for (j = 0; j < 10; j++)
 		{
 			_putchar(c2);
 			c2 += c;
@@ -29,10 +29,10 @@ void times_table(void)
 					_putchar(' ');
 				}
 			}
-			c++;
-			c2 = 0;
-			_putchar('$');
-			_putchar(' ');
 		}
+		c++;
+		c2 = 0;
+		_putchar('$');
+		_putchar('\n');
 	}
 }
