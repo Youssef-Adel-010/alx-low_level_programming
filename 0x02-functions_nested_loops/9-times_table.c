f8a71899b2a37057d92f5b5bb9acf0b0768e5d67
youssef adel <yossefadel260@gmail.com>
2023-03-22 13:33:40 -0700

    words

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,7 +9,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
 		for (mult = 1; mult <= 9; mult++)
@@ -23,7 +23,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48);
-			_putchar((prod / 10) + 48);
+			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
