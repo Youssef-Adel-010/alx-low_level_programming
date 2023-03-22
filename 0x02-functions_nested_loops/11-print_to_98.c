24006ae7a80e7b0c051bfa5d7c65e6de2176396d
youssef adel <yossefadel260@gmail.com>
2023-03-22 13:24:34 -0700

    words

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,6 +9,7 @@
 void print_to_98(int n)
 {
 	int i;
+
 	if (n > 98)
 	{
 		for (i = n; i > 98; i--)
@@ -22,6 +23,6 @@ void print_to_98(int n)
 		{
 			printf("%d, ", i);
 		}
-	}	
+	}
 	printf("98\n");
 }
